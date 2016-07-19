d8459c2749f27b3a1cb2e0deeaf8dc0328eee77c
mikeplavsky <mike.plavsky@quest.com>
2016-07-19 13:07:04 +0300

    getting versions

--- sync.py
+++ sync.py
@@ -3,8 +3,14 @@ import boto3
 def run():
 
     l = boto3.client("lambda")
-    f = l.get_function(FunctionName="aws-cleaner-sg")
 
-    print(f)
+    vs = l.list_versions_by_function(
+            FunctionName="aws-cleaner-sg")
+
+    print(len(vs["Versions"]))
+
+    for v in vs["Versions"]:
+        print(v["FunctionArn"])
+
 
 
