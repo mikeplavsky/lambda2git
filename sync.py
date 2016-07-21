6c7fde2d31932f60781e922b192dbf94fec14cfd
mikeplavsky <mike.plavsky@quest.com>
2016-07-21 12:20:46 +0300

    preparing for the next step - saving to github

--- sync.py
+++ sync.py
@@ -3,9 +3,38 @@ import urllib2
 import zipfile
 from tempfile import NamedTemporaryFile
 
-def run():
+l = boto3.client("lambda")
 
-    l = boto3.client("lambda")
+def get_function(fn):
+
+    f = l.get_function(
+            FunctionName=fn)
+
+    loc = f["Code"]["Location"]
+    print(loc)
+    
+    code = urllib2.urlopen(loc)
+    zip_code = code.read()
+
+    tf = NamedTemporaryFile()
+
+    tf.write(zip_code)
+    tf.flush()
+
+    import zipfile
+
+    z = zipfile.ZipFile(tf.name, 'r', zipfile.ZIP_STORED)
+    z.extractall(path=tf.name + ".zip")
+
+    tf.close()
+
+    ns = z.namelist()
+
+    zf = open(tf.name + ".zip/" + ns[0])
+    print(zf.read())
+
+
+def run():
 
     vs = l.list_versions_by_function(
             FunctionName="aws-cleaner-sg")
@@ -21,31 +50,7 @@ def run():
         print(v["Version"])
         print(v["Description"])
 
-        f = l.get_function(
-                FunctionName=fn)
-
-        loc = f["Code"]["Location"]
-        print(loc)
-        
-        code = urllib2.urlopen(loc)
-        zip_code = code.read()
-
-        tf = NamedTemporaryFile()
-
-        tf.write(zip_code)
-        tf.flush()
-
-        import zipfile
-
-        z = zipfile.ZipFile(tf.name, 'r', zipfile.ZIP_STORED)
-        z.extractall(path=tf.name + ".zip")
-
-        tf.close()
-
-        ns = z.namelist()
-
-        zf = open(tf.name + ".zip/" + ns[0])
-        print(zf.read())
+        get_function(fn)
 
 
 
