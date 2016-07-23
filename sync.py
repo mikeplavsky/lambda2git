22f8b49695b7a2e895fc2ff84a749dcbf42e7fca
mikeplavsky <mike.plavsky@quest.com>
2016-07-23 12:47:55 +0300

    updating file in git

--- sync.py
+++ sync.py
@@ -7,17 +7,28 @@ import os
 
 git_user = os.environ.get("GIT_USER")
 git_key = os.environ.get("GIT_KEY")
+git_repo = os.environ.get("GIT_REPO")
 
-git_url = "https://api.github.com/repos/GitQuest/aws-police/contents/README.md"
+git_url = "https://api.github.com/repos/%s/contents/%s"
 
 l = boto3.client("lambda")
 
 def create_file():
+
+    text = "it is cool!"
+    import base64
     
     import requests
-    res = requests.get(
-            git_url,
-            auth=(git_user,git_key))
+
+    res = requests.put(
+            
+            git_url % (git_repo, "README1.md"),
+            auth=(git_user,git_key),
+
+            json=dict(
+                message="checking...",
+                sha = "419861053327af2a12d5c8769153a4571da2f96b",
+                content=base64.b64encode(text)))
 
     return res
 
